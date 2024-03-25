ac4ae85063b79b03323e9e8fb29342135a795b61
hhbrenda <ojatammb@gmail.com>
2024-03-25 18:58:35 +0000

    done content page and team page fixes

--- src/contentStudent3/content3.js
+++ src/contentStudent3/content3.js
@@ -1,13 +1,3 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scrollToTopBtn").style.display = "block";
-    } else {
-        document.getElementById("scrollToTopBtn").style.display = "none";
-    }
-};
-
-// When the user clicks on the button, scroll to the top of the document
 document.getElementById("scrollToTopBtn").addEventListener("click", function() {
-    window.scrollTo({top: 0, behavior: 'smooth'}); // For a smooth scrolling effect
+    window.scrollTo({top: 0, behavior: 'smooth'}); 
 });
